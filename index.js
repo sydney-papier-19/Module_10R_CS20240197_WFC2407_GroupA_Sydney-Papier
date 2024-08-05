32d0fa81d3244271bde01916c7deae530f03a007
sydney_p <sydneypapier23@gmail.com>
2024-08-05 04:04:44 +0200

    added favicon to header and viewport

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref, push, onValue } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = { 
     databaseURL: "https://playground-4febd-default-rtdb.europe-west1.firebasedatabase.app/"
@@ -20,9 +20,20 @@ addButtonEl.addEventListener("click", function() {
 
     clearInputFieldEl()
 
-    appendItemToShoppingListEl(inputValue)
     
 })
+
+onValue(shoppingListInDB, function(snapshot)) {
+    let itemsArray = Object.values(snapshot.val())
+
+    shoppingListEl.innerHTML = ""
+
+    for (let i =0; i< itemsArray.length; i++){
+        appendItemToShoppingListEl(itemsArray[i])
+    }
+
+    console.log(itemsArray)
+}
 
 function clearInputFieldEl() {
     inputFieldEl.value = ""
