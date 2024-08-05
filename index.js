0152f7d35fd88389880be591d3ddba3e75598fde
sydney_p <sydneypapier23@gmail.com>
2024-08-05 10:33:50 +0200

    Added more commenting to html and js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+/*imports firebase app and realtime database libraries */
+
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js"
 import { getDatabase, ref, push, onValue, remove } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-database.js"
-
+/*initializes the firebase app with this database URL */
 const appSettings = { 
     databaseURL: "https://playground-2-f1680-default-rtdb.firebaseio.com/"
 }
@@ -8,21 +10,21 @@ const appSettings = {
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
 const shoppingListInDB = ref(database, "shoppingList")
-
+/*getting references to DOM elements in the html document using document.getElementById */
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 const shoppingListEl = document.getElementById("shopping-list")
-
+/* adds evet listener to add-btn */
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    push(shoppingListInDB, inputValue)
+    push(shoppingListInDB, inputValue) /*pushes the value to the shoppingList node in firebase database*/
    
-    clearInputFieldEl()
+    clearInputFieldEl() /*clears the input field */
 })
 
 onValue(shoppingListInDB, function(snapshot) {
-    if (snapshot.exists()) {
+    if (snapshot.exists()) {  //checks if snapshot exists
         let itemsArray = Object.entries(snapshot.val()) 
 
         clearShoppingListEl()
@@ -35,19 +37,19 @@ onValue(shoppingListInDB, function(snapshot) {
             appendItemToShoppingListEl(currentItem)
         }
     } else {
-        shoppingListEl.innerHTML = "No items here... yet"
+        shoppingListEl.innerHTML = "No items here... yet" //if no items display this message
     }
 })
 
-function clearShoppingListEl() {
+function clearShoppingListEl() { //clears the shopping list
     shoppingListEl.innerHTML = ""
 }
 
-function clearInputFieldEl() {
+function clearInputFieldEl() {  //cleas the input field's value
     inputFieldEl.value = ""
 }
 
-function appendItemToShoppingListEl(item) {
+function appendItemToShoppingListEl(item) {  //creates a new list item element
     let itemID = item[0]
     let itemValue = item[1]
     
